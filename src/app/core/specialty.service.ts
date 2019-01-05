6d79b8a1a10f0b8e317f2051ad0210ff2f661f87
Joël Arnold <joel.arnold@gmail.com>
2019-01-05 17:17:54 +0100

    Use standard dose from specialty on specialty changes.

--- src/app/core/specialty.service.ts
+++ src/app/core/specialty.service.ts
@@ -7,6 +7,9 @@ import { Specialty } from './specialty.model';
 export class SpecialtyService {
 
   getSpecialties(): Specialty[] {
-    return [new Specialty('Clindamycine 125mg/5ml', 25, 15), new Specialty('Clindamycine 250mg/5ml', 50, 15)];
+    return [
+      new Specialty('Clindamycine 125mg/5ml', 25, 15),
+      new Specialty('Clindamycine 250mg/5ml', 50, 20),
+    ];
   }
 }
--- src/app/pharma/formula/formula.component.ts
+++ src/app/pharma/formula/formula.component.ts
@@ -21,21 +21,61 @@ export class FormulaComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private specialtyService: SpecialtyService, private formulaService: FormulaService) {
     this.specialties = specialtyService.getSpecialties();
     this.form = this.formBuilder.group({
-      specialty: this.specialties[0],
+      specialty: '',
       weight: '',
-      targetDose: 15,
+      targetDose: '',
       intakes: 3,
     });
   }
 
   ngOnInit() {
-    this.update(this.form.value);
-    this.form.valueChanges.subscribe((formObject) => {
-      this.update(formObject);
+    this.updateAll(this.form.value);
+    this.form.get('specialty').valueChanges.subscribe((specialty) => {
+      this.updateSpecialty(specialty);
+    });
+    this.form.get('weight').valueChanges.subscribe((weight) => {
+      this.updateWeight(weight);
+    });
+    this.form.get('targetDose').valueChanges.subscribe((targetDose) => {
+      this.updateTargetDose(targetDose);
+    });
+    this.form.get('intakes').valueChanges.subscribe((intakes) => {
+      this.updateIntakes(intakes);
     });
   }
 
-  update(formObject: object) {
+  updateSpecialty(specialty: Specialty) {
+    if (specialty) {
+      const currentForm = this.form.value;
+      currentForm['specialty'] = specialty;
+      currentForm['targetDose'] = specialty.standardDose;
+      this.form.get('targetDose').setValue(specialty.standardDose);
+      this.updateAll(currentForm);
+    }
+  }
+  updateWeight(weight: number) {
+    if (weight) {
+      const currentForm = this.form.value;
+      currentForm['weight'] = weight;
+      this.updateAll(currentForm);
+    }
+  }
+  updateTargetDose(targetDose: number) {
+    if (targetDose) {
+      const currentForm = this.form.value;
+      currentForm['targetDose'] = targetDose;
+      this.updateAll(currentForm);
+    }
+  }
+  updateIntakes(intakes: number) {
+    if (intakes) {
+      const currentForm = this.form.value;
+      currentForm['intakes'] = intakes;
+      this.updateAll(currentForm);
+    }
+  }
+
+  updateAll(formObject: object) {
     if (formObject) {
       [this.dailyMl, this.intakeMl] = this.formulaService.compute(
         formObject['specialty'].mgPerMl,
